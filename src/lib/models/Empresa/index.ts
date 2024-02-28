385c311a2f13680fab30f138b40e6d3272ad44df
comptels <santiago.ruiz1991@gmail.com>
2024-02-28 17:51:54 -0500

    fix required models and interface enterprise.

--- src/lib/models/Empresa/index.ts
+++ src/lib/models/Empresa/index.ts
@@ -1,15 +1,15 @@
 import type { TablasType } from "$/lib/models/Tablas/Tabla";
 
 export interface EmpresaModelInterface {
-    estado?: TablasType
-    tipo_doc?: TablasType
-    doc?: string
-    nombre?: string
-    nombre_comercial?: string
-    actividad_eco?: TablasType
-    resp_fiscal?: TablasType
-    impuestos?: TablasType
-    ambiente?: TablasType
+    estado: TablasType
+    tipo_doc: TablasType
+    doc: string
+    nombre: string
+    nombre_comercial: string
+    actividad_eco: TablasType
+    resp_fiscal: TablasType
+    impuestos: TablasType
+    ambiente: TablasType
     id_software?: string
     token?: string
     token_pass?: string
